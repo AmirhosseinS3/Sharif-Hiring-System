2a4aa10151588d812079a930f2dd97cb804f16ab
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-01-02 11:30:18 +0330

    Templates added

--- SHS/urls.py
+++ SHS/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/hiring', include('hiring.urls'))
+    path('hiring/', include('hiring.urls')),
 ]
--- hiring/views.py
+++ hiring/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def hiring(request):
+    return render(request, 'login.html')
