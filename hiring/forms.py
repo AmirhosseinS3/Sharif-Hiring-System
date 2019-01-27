54a4c88db7d99e24dedbbb2a249f8e6afdb4fcce
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-01-28 00:56:46 +0330

    Sign up test succesful

--- hiring/forms.py
+++ hiring/forms.py
@@ -6,6 +6,7 @@ from .models import Employee, Employer
 
 class SignUpEmployerForm(UserCreationForm):
     name = forms.CharField(max_length=30, label='نام شرکت')
+    username = forms.CharField(max_length=30, label='نام کاربری')
     email = forms.EmailField(max_length=254, label='ایمیل')
     address = forms.CharField(max_length=500, label='آدرس')
     description = forms.CharField(max_length=2000, label='توضیحات شرکت')
@@ -22,23 +23,15 @@ class SignUpEmployerForm(UserCreationForm):
         help_text='رمز عبور قبلی را دوباره وارد کنید',
     )
 
-    def save(self, commit=True):
-        user = super().save()
-        name = self.cleaned_data['name']
-        address = self.cleaned_data['address']
-        description = self.cleaned_data['description']
-        employer = Employer(name=name, address=address, description=description, username=user.id)
-        employer.save()
-
     class Meta:
         model = Employer
-        fields = ('name', 'address', 'email', 'description', 'password1', 'password2',)
+        fields = ('name', 'username', 'address', 'email', 'description', 'password1', 'password2',)
 
 
 class SignUpEmployeeForm(UserCreationForm):
     name = forms.CharField(max_length=30, label='نام و نام خانوادگی')
     email = forms.EmailField(max_length=254, label='ایمیل')
-    SSN = forms.IntegerField(label='شماره ملی')
+    username = forms.CharField(label='شماره ملی')
     password1 = forms.CharField(
         label='رمز عبور',
         strip=False,
@@ -54,10 +47,5 @@ class SignUpEmployeeForm(UserCreationForm):
 
     class Meta:
         model = Employee
-        fields = ('name', 'email', 'password1', 'password2',)
+        fields = ('name', 'username', 'email', 'password1', 'password2',)
 
-    # def save(self, commit=True):
-    #     user = super().save()
-    #     name = self.cleaned_data['name']
-    #     employer = Employee(name=name, username=user.username, email=user.email, password=user.password)
-    #     employer.save()
--- hiring/models.py
+++ hiring/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Employee(User):
     name = models.CharField(max_length=30, null=False)
-    SSN = models.IntegerField(null=False, primary_key=True)
 
 
 class Employer(User):
