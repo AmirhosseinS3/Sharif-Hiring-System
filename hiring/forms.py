297980ec926bae4b5e364cb1f662a3c74c4a1b43
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-01-28 01:01:55 +0330

    Minor improvements for sign up

--- hiring/forms.py
+++ hiring/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from .models import Employee, Employer
 
@@ -10,18 +9,11 @@ class SignUpEmployerForm(UserCreationForm):
     email = forms.EmailField(max_length=254, label='ایمیل')
     address = forms.CharField(max_length=500, label='آدرس')
     description = forms.CharField(max_length=2000, label='توضیحات شرکت')
-    password1 = forms.CharField(
-        label='رمز عبور',
-        strip=False,
-        widget=forms.PasswordInput,
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label='تکرار رمز عبور',
-        widget=forms.PasswordInput,
-        strip=False,
-        help_text='رمز عبور قبلی را دوباره وارد کنید',
-    )
+
+    def __init__(self):
+        super().__init__()
+        self.password1.label = 'رمز عبور'
+        self.password2.label = 'تکرار رمز عبور'
 
     class Meta:
         model = Employer
@@ -32,20 +24,12 @@ class SignUpEmployeeForm(UserCreationForm):
     name = forms.CharField(max_length=30, label='نام و نام خانوادگی')
     email = forms.EmailField(max_length=254, label='ایمیل')
     username = forms.CharField(label='شماره ملی')
-    password1 = forms.CharField(
-        label='رمز عبور',
-        strip=False,
-        widget=forms.PasswordInput,
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label='تکرار رمز عبور',
-        widget=forms.PasswordInput,
-        strip=False,
-        help_text='رمز عبور قبلی را دوباره وارد کنید',
-    )
+
+    def __init__(self):
+        super().__init__()
+        self.password1.label = 'رمز عبور'
+        self.password2.label = 'تکرار رمز عبور'
 
     class Meta:
         model = Employee
         fields = ('name', 'username', 'email', 'password1', 'password2',)
-
