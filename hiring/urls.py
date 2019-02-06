a734ea6312f058d25b2674e411cb4271041033ba
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-02-06 10:12:12 +0330

    All employees url added

--- hiring/urls.py
+++ hiring/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
     path(r'announcement_comment/<int:id>', views.announcement_comment, name="announcement_comment"),
     path(r'employee_comment/<int:id>', views.employee_comment, name="employee_comment"),
     path(r'success_comment', views.success_comment, name="success_comment"),
-    path(r'employee_page/<int:id>', views.employee_page, name='employee_page')
+    path(r'employee_page/<int:id>', views.employee_page, name='employee_page'),
+    path(r'all_employees/<int:id>', views.all_employees, name='all_employees'),
 ]
